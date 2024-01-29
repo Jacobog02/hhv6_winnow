679cfe7621a930b106880a541a679c160d264194
Jacobog02 <jacobog02@gmail.com>
2024-01-29 22:49:50 +0000

    Added meryl k=15-mer indexes for minnowmap2 alignment

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ HHV6A: https://www.ncbi.nlm.nih.gov/nuccore/NC_001664.4
 
 HHV6B: https://www.ncbi.nlm.nih.gov/nuccore/NC_000898
 
+JACOB! 
+I needed a recomputable way to redo these hhv6 indexes its too complicated...
+Both indexes indexed independently and matched to appropriate sample... 
+
+Mapping using the built in meryl and minnowmap2 implementation for their install
+Uhhh I basically went into each directory and did `bash ../index_winnowmap.sh genome.fasta` took like 2 seconds
+
 
---
+++ index_winnowmap.sh
@@ -0,0 +1,16 @@
+## Jacob G | Lareau Lab | Jan 29 2023
+## Automated script to take a cmd line argument and then winnowmap index that badboy.
+## Done using the meryl k-mer counting tool idk man I manually set the winnowmap path bc this will be deployed on a job eventually
+winnow=~/bin/Winnowmap/bin/
+
+in_fa=$1
+the_k=15 ## why not 9-mers???
+
+## mapping 15k mers??? IDK HOW TO CHECK WHA
+$winnow/meryl count k=$the_k output merylDB $in_fa
+$winnow/meryl print greater-than distinct=0.9998 merylDB > ${in_fa}.rep_k${the_k}.txt
+
+
+## now you can supposedly align via
+#$winnow/winnowmap -W ${in_fa}.rep_k15.txt -ax map-pb $in_fa hifi.fq.gz > output.sam
+
